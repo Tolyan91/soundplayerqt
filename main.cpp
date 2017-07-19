27a7ede06851a04456a5a7d062a846153f95749f
Tolyan91 <klaviat91@mail.ru>
2017-07-19 23:17:20 +0700

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+#include "soundplayer.h"
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    SoundPlayer soundPlayer;
+
+    soundPlayer.resize(320, 75);
+    soundPlayer.show();
+
+    return app.exec();
+}
